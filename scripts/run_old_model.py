6b6450dfe6d451c2305f5b65e49991e8edff186e
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-05-10 18:23:36 -0500

    ✨ Create files that run both models directly, enhancing reproducibility

---
+++ scripts/run_old_model.py
@@ -0,0 +1,42 @@
+import numpy as np
+import warnings
+
+from time_domain_ccst.fem_solver import retrieve_solution
+
+warnings.filterwarnings(
+    "ignore", "The following kwargs were not used by contour: 'shading'", UserWarning
+)  # ignore unimportant warning from solidspy
+
+
+def plot_fields(bc_array, nodes, elements, solution):
+    import solidspy.postprocesor as pos
+    import matplotlib.pyplot as plt
+
+    sol_displacement = pos.complete_disp(bc_array[:, :2], nodes, solution, ndof_node=3)
+    pos.plot_node_field(
+        sol_displacement[:, 0], nodes, elements, title="X"
+    )  # x component
+    pos.plot_node_field(
+        sol_displacement[:, 1], nodes, elements, title="Y"
+    )  # y component
+    pos.plot_node_field(
+        sol_displacement[:, 3], nodes, elements, title=r"$\theta$"
+    )  # y component
+    plt.show()
+
+
+def main():
+    geometry_type = "square"
+    params = {"side": 1.0, "mesh_size": 1.0}
+    force_reprocess = True
+    cst_model = "cst_quad9"
+
+    bc_array, solution, nodes, elements = retrieve_solution(
+        geometry_type, params, cst_model, force_reprocess=force_reprocess
+    )
+
+    plot_fields(bc_array, nodes, elements, solution)
+
+
+if __name__ == "__main__":
+    main()
