dc8da2c177d3f801f64afce28a2727486846930a
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-06-01 15:24:04 -0500

    ✨ Add scripts to solve eigenvalues problem for both cases

---
+++ scripts/eigsolve_old_model.py
@@ -0,0 +1,69 @@
+import warnings
+
+import numpy as np
+
+from time_domain_ccst.fem_solver import retrieve_solution
+
+warnings.filterwarnings(
+    "ignore", "The following kwargs were not used by contour: 'shading'", UserWarning
+)  # ignore unimportant warning from solidspy
+
+
+def plot_fields_quad9_rot4(
+    bc_array: np.array, nodes: np.array, elements: np.array, solution: np.array
+) -> None:
+    import matplotlib.pyplot as plt
+    import solidspy.postprocesor as pos
+
+    sol_displacement = pos.complete_disp(bc_array[:, :2], nodes, solution, ndof_node=2)
+    pos.plot_node_field(
+        sol_displacement[:, 0], nodes, elements, title="X"
+    )  # x component
+    pos.plot_node_field(
+        sol_displacement[:, 1], nodes, elements, title="Y"
+    )  # y component
+
+    # and plot the rotations field
+    vertex_nodes = list(set(elements[:, 3:7].flatten()))
+    sol_rotation = pos.complete_disp(
+        bc_array[vertex_nodes, 2].reshape(-1, 1),
+        nodes[vertex_nodes],
+        solution,
+        ndof_node=1,
+    )
+
+    x = nodes[vertex_nodes][:, 1]
+    y = nodes[vertex_nodes][:, 2]
+    z = sol_rotation.flatten()
+
+    fig, ax = plt.subplots()
+    # Create a contour plot
+    contour = ax.tricontourf(x, y, z, levels=50, cmap="viridis")
+    fig.colorbar(contour, ax=ax)
+    plt.title("W")
+    plt.show()
+
+
+def main():
+    geometry_type = "square"
+    params = {"side": 1.0, "mesh_size": 0.1}
+    force_reprocess = True
+    cst_model = "cst_quad9"
+    constraints_loads = "borders_fixed"
+    eigsolution = True
+
+    bc_array, eigvals, eigvecs, nodes, elements = retrieve_solution(
+        geometry_type,
+        params,
+        cst_model,
+        constraints_loads,
+        eigensolution=eigsolution,
+        force_reprocess=force_reprocess,
+    )
+
+    n_eigvec = 0
+    plot_fields_quad9_rot4(bc_array, nodes, elements, eigvecs[:, n_eigvec])
+
+
+if __name__ == "__main__":
+    main()
