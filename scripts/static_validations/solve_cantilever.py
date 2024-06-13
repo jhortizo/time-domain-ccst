a2826bc2903385d846343a115686d337e2b18fc9
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-06-13 09:58:59 -0500

    🧐 Prepare comparisons for different h/l ratios static case

--- scripts/static_validations/solve_cantilever.py
+++ scripts/static_validations/solve_cantilever.py
@@ -1,37 +1,79 @@
 import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 from time_domain_ccst.fem_solver import retrieve_solution
 from time_domain_ccst.plotter import plot_fields_quad9_rot4
 
+from solidspy.postprocesor import complete_disp, plot_node_field
+
 
 def main():
     geometry_type = "rectangle"
-    params = {"side_y": 1.0, "side_x": 20.0, "mesh_size": 0.4}
-    force_reprocess = True
+    force_reprocess = False
     cst_model = "cst_quad9_rot4"
     constraints_loads = "cantilever_support_load"
 
-    materials = np.array(
-        [
-            [
-                1,  # E, young's modulus
-                0.29,  # nu, poisson's ratio
-                1,  # eta, coupling parameter
-                1,  # rho, density
-            ]
-        ]
-    )
+    E = 1
+    nu = 0
+    rho = 1
+    mu = E / (2 * (1 + nu))
 
-    bc_array, solution, nodes, elements = retrieve_solution(
-        geometry_type,
-        params,
-        cst_model,
-        constraints_loads,
-        materials,
-        force_reprocess=force_reprocess,
-    )
+    h = 1
+    L = 20 * h
+    params = {"side_y": h, "side_x": L, "mesh_size": 0.1}
 
-    plot_fields_quad9_rot4(bc_array, nodes, elements, solution)
+    h_l_ratios = np.array([1e4, 2, 1e-4])
+    ls = h / h_l_ratios
+    etas = ls**2 * mu
+
+    u_lowers = []
+    nodes_lowers = []
+    for i in tqdm(range(len(h_l_ratios)), desc="h/l ratios"):
+        materials = np.array([[E, nu, etas[i], rho]])
+        custom_str = f"h_l_ratio_{h_l_ratios[i]:.2e}"
+
+        bc_array, solution, nodes, elements = retrieve_solution(
+            geometry_type,
+            params,
+            cst_model,
+            constraints_loads,
+            materials,
+            force_reprocess=force_reprocess,
+            custom_str=custom_str,
+        )
+
+        u = complete_disp(bc_array, nodes, solution, ndof_node=2)
+
+        low_border_ids = np.where(nodes[:, 2] == 0)[0]
+
+        nodes_lower = nodes[low_border_ids]
+        u_lower = u[low_border_ids]
+
+        nodes_lowers.append(nodes_lower)
+        u_lowers.append(u_lower)
+
+    max_u_lower_val = 5e4 # TODO: des machetize this
+
+    norm_u_lowers = [u_lower / max_u_lower_val for u_lower in u_lowers]
+
+    plt.rcParams["font.size"] = 24
+    plt.figure(figsize=(12, 6))
+
+    for i in range(len(h_l_ratios)):
+        plt.plot(
+            nodes_lowers[i][:, 1],
+            norm_u_lowers[i][:, 0],
+            ".",
+            label=f"h/l = {h_l_ratios[i]:.0e}",
+        )
+
+    plt.xlabel("x")
+    plt.ylabel("Vertical displacement")
+    plt.legend()
+    plt.grid()
+    plt.savefig("compare_cantilever_displacement.png", dpi=300) # TODO: save in proper folder
+    plt.show()
 
 
 if __name__ == "__main__":
--- time_domain_ccst/constraints_loads_creators.py
+++ time_domain_ccst/constraints_loads_creators.py
@@ -84,7 +84,7 @@ def cantilever_support_load(line3, cell_data, npts):
     loads = np.zeros((npts, 4))  # empty loads
     loads[:, 0] = np.arange(npts)  # specify nodes
 
-    loads[list(right_border), 1 + 1] = -100  # force in y direction
+    loads[list(right_border), 1 + 1] = -1  # force in y direction
 
     return cons, loads
 
