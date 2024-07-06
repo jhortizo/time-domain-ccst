9fc48e3d3f483b54bbb42f93405811c3dea33dbf
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-07-06 09:50:35 -0500

    ✨ Fix up to the classical eigenproblem solution

--- scripts/time_marching/classical_time_marching.py
+++ scripts/time_marching/classical_time_marching.py
@@ -6,15 +6,15 @@ initial state an eigenvector of the system, and without loads.
 import numpy as np
 
 from time_domain_ccst.fem_solver import retrieve_solution
-from time_domain_ccst.plotter import plot_fields_quad9_rot4
+from time_domain_ccst.plotter import plot_fields_classical
 
 
 def main():
     geometry_type = "rectangle"
-    params = {"side_x": 1.0, "side_y": 10.0, "mesh_size": 0.1}
+    params = {"side_x": 10.0, "side_y": 1.0, "mesh_size": 1.0}
     force_reprocess = True
-    cst_model = "classical"
-    constraints_loads = "cantilever_support"
+    cst_model = "classical_quad9"
+    constraints_loads = "cantilever_support_classical"
 
     materials = np.array(
         [
@@ -27,7 +27,7 @@ def main():
     )
 
     # first solve the eigenvalue problem and acquire an eigenstate
-    eigsolution = True
+    scenario_to_solve = "eigenproblem"
 
     bc_array, eigvals, eigvecs, nodes, elements = retrieve_solution(
         geometry_type,
@@ -35,26 +35,32 @@ def main():
         cst_model,
         constraints_loads,
         materials,
-        eigensolution=eigsolution,
+        scenario_to_solve="eigenproblem",
         force_reprocess=force_reprocess,
     )
 
-    n_eigvec = 0
-    plot_fields_quad9_rot4(bc_array, nodes, elements, eigvecs[:, n_eigvec], show=True)
+    n_eigvec = 5
+    plot_fields_classical(bc_array, nodes, elements, eigvecs[:, n_eigvec], instant_show=True)
 
     # the mesh and constraints are the same, so the exact structure of the eigvecs array
     # can be used as initial state
+
+    scenario_to_solve = "time-marching"
     bc_array, solutions, nodes, elements = retrieve_solution(
         geometry_type,
         params,
         cst_model,
         constraints_loads,
         materials,
+        scenario_to_solve=scenario_to_solve,
         force_reprocess=force_reprocess,
         dt=0.1,
         n_t_iter=10,
         initial_state=eigvecs[:, 0],
     )
+
+    for i in range(10):
+        plot_fields_classical(bc_array, nodes, elements, solutions[i], show=True)
 
     pass
 
