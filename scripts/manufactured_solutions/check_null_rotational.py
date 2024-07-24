b3f3acd427f9e4d093d49327ce7101ce0af25ccc
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-07-23 22:48:56 -0500

    🧪 Add easy check for boundary conditions as well

--- scripts/manufactured_solutions/check_null_rotational.py
+++ scripts/manufactured_solutions/check_null_rotational.py
@@ -1,5 +1,5 @@
-from time_domain_ccst.mms.proposed_solutions import manufactured_solution_1
 from continuum_mechanics import vector
+from time_domain_ccst.mms.proposed_solutions import manufactured_solution_2
 
 
 def check_null_rotational(manufactured_solution: callable) -> None:
@@ -12,5 +12,23 @@ def check_null_rotational(manufactured_solution: callable) -> None:
     assert curl_u[2] == 0
 
 
+def check_boundary_conditions(manufactured_solution: callable) -> None:
+    import sympy as sp
+    x, y = sp.symbols("x y")
+    u, u_fnc = manufactured_solution()
+
+    u_x = u[0]
+    u_y = u[1]
+
+    assert u_x.subs(x, 0) == 0
+    assert u_x.subs(x, 1) == 0
+    assert u_y.subs(y, 0) == 0
+    assert u_y.subs(y, 1) == 0
+
+
 if __name__ == "__main__":
-    check_null_rotational(manufactured_solution_1)
+    
+    fcn_to_check = manufactured_solution_2
+    
+    check_null_rotational(fcn_to_check)
+    check_boundary_conditions(fcn_to_check)
