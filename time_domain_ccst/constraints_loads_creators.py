💩 Fix force and rigidity calculation to derive results from reference paper (#10)
* 🎨 Refactor some lines

* 🎨 Refactor some lines

* 💩 Adjust force and rigidity calculation to correct plot

time_domain_ccst/constraints_loads_creators.py
@@ -75,16 +75,16 @@ def cantilever_support_load(line3, cell_data, npts, load=-1):
     TODO: properly describe this, maybe add some figures to illustrate...
     """
 
-    left_border = set(line3[cell_data["line3"]["gmsh:physical"] == 4].flatten())
-    right_border = set(line3[cell_data["line3"]["gmsh:physical"] == 2].flatten())
+    left_border = list(set(line3[cell_data["line3"]["gmsh:physical"] == 4].flatten()))
+    right_border = list(set(line3[cell_data["line3"]["gmsh:physical"] == 2].flatten()))
 
     cons = np.zeros((npts, 3), dtype=int)
-    cons[list(left_border), :] = -1
+    cons[left_border, :] = -1
 
     loads = np.zeros((npts, 4))  # empty loads
     loads[:, 0] = np.arange(npts)  # specify nodes
 
-    loads[list(right_border), 1 + 1] = load  # force in y direction
+    loads[right_border, 1 + 1] = load * len(right_border) # force in y direction, multiplied by number of points
 
     return cons, loads
 