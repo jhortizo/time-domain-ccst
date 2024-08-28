1db3cabcb8b2f91ac0e4dfe41d06841eaee19ad1
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-08-28 10:00:56 -0500

    💄 Style plots for static mms and calculate slope interactively

--- time_domain_ccst/mms/plots.py
+++ time_domain_ccst/mms/plots.py
@@ -5,6 +5,18 @@ import numpy as np
 from solidspy.postprocesor import complete_disp, plot_node_field
 from time_domain_ccst.constants import IMAGES_FOLDER
 
+# rewrites the settings from solidspy.postprocessor
+black = "#000000"
+plt.rcParams["image.cmap"] = "YlGnBu_r"
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams["text.color"] = black
+plt.rcParams["font.size"] = 12
+plt.rcParams["xtick.color"] = black
+plt.rcParams["ytick.color"] = black
+plt.rcParams["axes.labelcolor"] = black
+plt.rcParams["axes.edgecolor"] = black
+plt.rcParams["axes.spines.right"] = False
+plt.rcParams["axes.spines.top"] = False
 
 def conditional_loads_plotting(
     bc_array,
@@ -55,10 +67,28 @@ def conditional_fields_plotting(
             nodes,
             elements,
             savefigs=savefigs,
-            filename=[f"{IMAGES_FOLDER}/{image_names}_u_fem_{len(elements)}_elements.png"],
+            filename=[
+                f"{IMAGES_FOLDER}/{image_names}_u_fem_{len(elements)}_elements.png"
+            ],
         )
-        plot_node_field(u_true_norm, nodes, elements, savefigs=savefigs, filename=[f'{IMAGES_FOLDER}/{image_names}_u_true_{len(elements)}_elements.png'])
-        plot_node_field(norm_diff, nodes, elements, savefigs=savefigs, filename=[f'{IMAGES_FOLDER}/{image_names}_norm_diff_{len(elements)}_elements.png'])
+        plot_node_field(
+            u_true_norm,
+            nodes,
+            elements,
+            savefigs=savefigs,
+            filename=[
+                f"{IMAGES_FOLDER}/{image_names}_u_true_{len(elements)}_elements.png"
+            ],
+        )
+        plot_node_field(
+            norm_diff,
+            nodes,
+            elements,
+            savefigs=savefigs,
+            filename=[
+                f"{IMAGES_FOLDER}/{image_names}_norm_diff_{len(elements)}_elements.png"
+            ],
+        )
 
         vertex_nodes = list(set(elements[:, 3:7].flatten()))
         sol_rotation = complete_disp(
@@ -77,21 +107,32 @@ def conditional_fields_plotting(
         # Create a contour plot
         contour = ax.tricontourf(x, y, z_fem, levels=50, cmap="YlGnBu_r")
         fig.colorbar(contour, ax=ax)
-        plt.savefig(f"{IMAGES_FOLDER}/{image_names}_curl_fem_{len(elements)}_elements.png", dpi=300)
+        plt.savefig(
+            f"{IMAGES_FOLDER}/{image_names}_curl_fem_{len(elements)}_elements.png",
+            dpi=300,
+        )
 
         fig, ax = plt.subplots()
         # Create a contour plot
         contour = ax.tricontourf(x, y, z_teo, levels=50, cmap="YlGnBu_r")
         fig.colorbar(contour, ax=ax)
-        plt.savefig(f"{IMAGES_FOLDER}/{image_names}_curl_true_{len(elements)}_elements.png", dpi=300)
+        plt.savefig(
+            f"{IMAGES_FOLDER}/{image_names}_curl_true_{len(elements)}_elements.png",
+            dpi=300,
+        )
 
         fig, ax = plt.subplots()
         # Create a contour plot
-        contour = ax.tricontourf(x, y, np.abs(z_teo-z_fem), levels=50, cmap="YlGnBu_r")
+        contour = ax.tricontourf(
+            x, y, np.abs(z_teo - z_fem), levels=50, cmap="YlGnBu_r"
+        )
         fig.colorbar(contour, ax=ax)
-        plt.savefig(f"{IMAGES_FOLDER}/{image_names}_curl_diff_{len(elements)}_elements.png", dpi=300)
+        plt.savefig(
+            f"{IMAGES_FOLDER}/{image_names}_curl_diff_{len(elements)}_elements.png",
+            dpi=300,
+        )
 
-        # close all figures 
+        # close all figures
         plt.close("all")
 
     if plot_field == "all":
@@ -113,8 +154,6 @@ def convergence_plot(
     log_mesh = np.log10(n_elements)
     log_rmse = np.log10(errors)
 
-    slope = np.polyfit(log_mesh, log_rmse, 1)[0]
-
     # and then plot the results
     plt.figure()
     plt.loglog(n_elements, errors, "o-", label=error_metric_name)
@@ -122,11 +161,14 @@ def convergence_plot(
     plt.xlabel("Number of elements")
     plt.ylabel("Error")
     plt.grid()
-    plt.legend()
+    # plt.legend()
 
-    plt.text(0.5, 0.9, f"Slope: {round(slope,2)}", transform=plt.gca().transAxes)
     if filename:
         plt.savefig(f"{IMAGES_FOLDER}/{filename}", dpi=300)
     plt.show()
 
+    n_last_points = input(
+        "Select number of last points to include in the slope calculation: "
+    )
+    slope = np.polyfit(log_mesh[-n_last_points:], log_rmse[-n_last_points:], 1)[0]
     print("Slope:", slope)
