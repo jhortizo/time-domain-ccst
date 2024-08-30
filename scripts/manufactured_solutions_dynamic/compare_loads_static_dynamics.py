daa3d43e0306b517a936b89a408da03e89ed9618
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-08-30 18:15:40 -0500

    🎨  Also compare loads vs their symbolic expression

--- scripts/manufactured_solutions_dynamic/compare_loads_static_dynamics.py
+++ scripts/manufactured_solutions_dynamic/compare_loads_static_dynamics.py
@@ -1,10 +1,11 @@
 """
 This file exists only as a mean to verify that the loads generated in the mms static files are congruent with the ones
-in dynamic cases. It is not important, can and should be omitted in final versions of this repo, or converted into a test 
+in dynamic cases. It is not important, can and should be omitted in final versions of this repo, or converted into a test
 or something like that...
 """
 
 import numpy as np
+import sympy as sp
 from solidspy.assemutil import assembler, loadasem
 from solidspy.postprocesor import complete_disp, plot_node_field
 
@@ -34,7 +35,7 @@ from time_domain_ccst.mms_t.utils import (
     impose_body_force_loads as i_t,
 )
 
-mesh_size = 0.5
+mesh_size = 0.1
 E = 1
 nu = 0.3
 rho = 1
@@ -47,12 +48,16 @@ mats = [
     eta,
     rho,
 ]
+x, y = sp.symbols("x y")
 
 mats = np.array([mats])
 
 # static
 u_s, u_fcn_s, _ = ms_s()
 body_force_fcn_s, f_s = cb_s(u_s)
+
+sp.plotting.plot3d(f_s[0], (x, 0, 1), (y, 0, 1))
+
 cons, elements, nodes, loads = g_s(mesh_size, "bla.msh")
 loads_s = i_s(loads, nodes, body_force_fcn_s, elements)
 
@@ -61,9 +66,9 @@ assem_op, bc_array, neq = assem_op_cst_quad9_rot4(cons, elements)
 stiff_mat, mass_mat = assembler(
     elements, mats, nodes, neq, assem_op, uel=cst_quad9_rot4
 )
-rhs = loadasem(loads_s, bc_array, neq)
+rhs_s = loadasem(loads_s, bc_array, neq)
 
-rhs_s = mass_mat @ rhs
+rhs_s = mass_mat @ rhs_s
 
 loads_field = complete_disp(bc_array, nodes, rhs_s, ndof_node=2)
 plot_node_field(loads_field, nodes, elements, title=["loads_x", "loads_y "])
@@ -72,6 +77,8 @@ plot_node_field(loads_field, nodes, elements, title=["loads_x", "loads_y "])
 # dynamic
 u_t, u_fcn_t = ms_t()
 body_force_fcn_t, f_t = cb_t(u_t)
+
+sp.plotting.plot3d(f_t, (0, 1), (0, 1))
 
 cons, elements, nodes, loads = g_t(mesh_size, "bla2.msh")
 loads_t = i_t(loads, nodes, body_force_fcn_t, 0.0)
@@ -85,10 +92,5 @@ rhs = loadasem(loads_t, bc_array, neq)
 # approximation to eval the function weighted by the form functions
 rhs_t = mass_mat @ rhs
 
-from solidspy.postprocesor import complete_disp, plot_node_field
-
 loads_field = complete_disp(bc_array, nodes, rhs_t, ndof_node=2)
 plot_node_field(loads_field, nodes, elements, title=["loads_x", "loads_y "])
-
-
-pass
