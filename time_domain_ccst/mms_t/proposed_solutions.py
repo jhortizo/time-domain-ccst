42e2c5f32d6bd391e198c07726e3090ad357c099
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-08-31 11:49:55 -0500

    🎨 Add temporal component to manufactured solutions

--- time_domain_ccst/mms_t/proposed_solutions.py
+++ time_domain_ccst/mms_t/proposed_solutions.py
@@ -6,16 +6,16 @@ x, y, t = sp.symbols("x y t")
 def manufactured_solution_no_oscillations() -> tuple[sp.Matrix, callable]:
     u = sp.Matrix(
         [
-            (x - x**2) ** 2 * (y - y**2) ** 2,
-            (x - x**2) ** 2 * (y - y**2) ** 2,
+            (x - x**2) ** 2 * (y - y**2) ** 2 * sp.cos(2 * sp.pi * t),
+            (x - x**2) ** 2 * (y - y**2) ** 2 * sp.cos(2 * sp.pi * t),
             0,
         ]
     )
     # TODO: for some reason here the u.row_del(2) decided not to work, try and fix it
     u_2d = sp.Matrix(
         [
-            (x - x**2) ** 2 * (y - y**2) ** 2,
-            (x - x**2) ** 2 * (y - y**2) ** 2,
+            (x - x**2) ** 2 * (y - y**2) ** 2 * sp.cos(2 * sp.pi * t),
+            (x - x**2) ** 2 * (y - y**2) ** 2 * sp.cos(2 * sp.pi * t),
         ]
     )
 
