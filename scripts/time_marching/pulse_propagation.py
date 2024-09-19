350b1901ab761a1bb8bde888579257f52970b971
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2024-09-19 13:14:48 -0500

    ✨ Add static plot as well

--- scripts/time_marching/pulse_propagation.py
+++ scripts/time_marching/pulse_propagation.py
@@ -5,27 +5,28 @@ initial state an eigenvector of the system, and without loads.
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy as sp
+from matplotlib.animation import FuncAnimation
 from solidspy.postprocesor import complete_disp
 
+from time_domain_ccst.constants import IMAGES_FOLDER
 from time_domain_ccst.fem_solver import retrieve_solution
 from time_domain_ccst.plotter import (
     plot_fields_classical,
     plot_fields_quad9_rot4,
     plot_oscillatory_movement_sample_points_complete_animation_vs_classical,
 )
-from time_domain_ccst.constants import IMAGES_FOLDER
 
 plt.rcParams["image.cmap"] = "YlGnBu_r"
 plt.rcParams["mathtext.fontset"] = "cm"
 
 
-def create_pulse_function(plotting=False):
-    import sympy as sp
+def create_pulse_function(x_length, plotting=False):
 
     x, y = sp.symbols("x y")
 
     # gaussian pulse centered at x = 0.5
-    u_y = sp.exp(-100 * (x - 0.5) ** 2)
+    u_y = sp.exp(-100 * (x - x_length / 2) ** 2)
     # sp.plotting.plot3d(u_y, (x, 0, 1), (y, 0, 1), title="u_x")
     u_x = 0
 
@@ -67,11 +68,12 @@ def get_dynamic_solution(
         custom_str=custom_str,
     )
 
+    print("Number of elements:", len(elements))
     plot_fields_classical(
         bc_array,
         nodes,
         elements,
-        solutions[:, 100],
+        solutions[:, -1],
         instant_show=True,
     )
 
@@ -91,10 +93,16 @@ def get_displacements(bc_array, nodes, solutions, n_iter_t):
 
 
 def plot_and_animation(
-    n_t_iter, dt, nodes, classical_solution_displacements, ccst_solution_displacements
+    n_t_iter,
+    dt,
+    nodes,
+    classical_solution_displacements,
+    ccst_solution_displacements,
+    n_plots=None,
+    fps=10,
 ):
     # -- Fancy plotting
-    ts = np.linspace(0, n_t_iter * dt, n_t_iter)
+    t = np.linspace(0, n_t_iter * dt, n_t_iter)
 
     line_nodes_ids = np.where(np.abs(nodes[:, 2] - 0.5) < 0.01)[0]
     line_nodes_ids = line_nodes_ids[np.argsort(nodes[line_nodes_ids, 1])]
@@ -103,9 +111,24 @@ def plot_and_animation(
     cl_u_y = classical_solution_displacements[line_nodes_ids, 1, :]
     ccst_u_y = ccst_solution_displacements[line_nodes_ids, 1, :]
 
-    n_plots = 1000
-    t = ts
-    # and then it comes the animation, complete
+    # static plot
+    n_times = 4
+
+    fig, axs = plt.subplots(nrows=n_times, ncols=2, figsize=(6, 6))
+
+    times_to_plot = np.linspace(0, n_t_iter - 1, n_times, dtype=int)
+
+    for i, time in enumerate(times_to_plot):
+        axs[i, 0].plot(nodes[line_nodes_ids, 1], ccst_u_y[:, time], "k")
+        axs[i, 1].plot(nodes[line_nodes_ids, 1], cl_u_y[:, time], color="gray")
+
+        axs[i, 0].set_title(f"Time: {t[time]:.2f}")
+
+    plt.tight_layout()
+    plt.savefig(f"{IMAGES_FOLDER}/pulse_propagation.png")
+    plt.show()
+
+    # animation of section
     if n_plots is None:
         n_plots = len(t)
 
@@ -116,7 +139,7 @@ def plot_and_animation(
     ax.set_aspect("equal")
 
     ax.set_ylim(-1, 1)
-    ax.set_xlim(0, 1)
+    # ax.set_xlim(0, 1)
 
     (contour_ccst,) = ax.plot(
         nodes[line_nodes_ids, 1],
@@ -143,11 +166,9 @@ def plot_and_animation(
             time_text,
         )
 
-    from matplotlib.animation import FuncAnimation
 
-    ani = FuncAnimation(fig, update, frames=len(time_steps), blit=True, interval=50)
+    ani = FuncAnimation(fig, update, frames=n_plots, blit=True, interval=50)
 
-    fps = 10
     ani.save(
         f"{IMAGES_FOLDER}/pulse_propagation.gif",
         fps=fps,
@@ -157,12 +178,15 @@ def plot_and_animation(
 def main():
     # -- Different cases run in this script
 
+    t_final = 4.0
     dt = 0.01
+    n_t_iter = int(t_final / dt)
 
     # -- Overall constants
     geometry_type = "rectangle"
-    params = {"side_x": 1.0, "side_y": 1.0, "mesh_size": 0.25}
-    force_reprocess = False
+    x_length = 2.0
+    params = {"side_x": x_length, "side_y": 1.0, "mesh_size": 0.1}
+    force_reprocess = True
     plotting = False
 
     ccst_constraints_loads = "cantilever_support"
@@ -190,10 +214,8 @@ def main():
         ]
     )
 
-    n_t_iter = 1000
-
     # -- Finding initial states
-    common_initial_state = create_pulse_function(plotting)
+    common_initial_state = create_pulse_function(x_length, plotting)
 
     # -- Getting dynamic solutions
     # custom_str = f"pulse_n_t_iter_{n_t_iter}_dt_{dt}_eta_{ccst_materials[0, 2]}_ccst"
